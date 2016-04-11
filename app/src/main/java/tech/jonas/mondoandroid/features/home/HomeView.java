6c8c4bb5c7dbd2d5f6c45c465c1589eba5840fbd
Jonas Lüthke <joluet@users.noreply.github.com>
2016-04-11 20:31:25 +0100

    Added pull-to-refresh. Added animation for list items. (#3)
    Added pull-to-refresh. Added animation for list items. Also, upgraded gradle plugin.

--- app/src/main/java/tech/jonas/mondoandroid/features/home/HomeView.java
+++ app/src/main/java/tech/jonas/mondoandroid/features/home/HomeView.java
@@ -5,6 +5,8 @@ import java.util.List;
 import tech.jonas.mondoandroid.ui.model.Transaction;
 
 public interface HomeView {
+    void setIsLoading(boolean isLoading);
+
     void setTitle(String title);
 
     void startLoginActivity();
